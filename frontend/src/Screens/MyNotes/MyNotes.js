58eb895c8291d969de7d1ae2d3cb35ab7d207f7e
Prashant Goswami <87839549+prashantcoding@users.noreply.github.com>
2021-12-07 21:54:52 +0530

    Before Accordion

--- frontend/src/Screens/MyNotes/MyNotes.js
+++ frontend/src/Screens/MyNotes/MyNotes.js
@@ -1,18 +1,43 @@
 import React from "react";
 import MainScreen from "../../component/MainScreen/MainScreen";
-import {Link} from 'react-router-dom'
-import {Button} from 'react-bootstrap'
+import { Link } from "react-router-dom";
+import { Button, Card } from "react-bootstrap";
+import notes from "../../data/notes";
 const MyNotes = () => {
   return (
-  <MainScreen title="Welcome Back Prashant">
-    <Link to="/createnewnode">
-        <Button style={{marginLeft: 10,marginBottom: 6}} size='lg'>
-            Create New Note
+    <MainScreen title="Welcome Back Prashant...">
+      <Link to="/createnode"/>
+        <Button style={{ marginLeft: 10, marginBottom: 6 }} size="lg">
+          Create New Note
         </Button>
-
-    </Link>
-
+        {notes.map(note=>(
+            <Card style={{ margin: 10 }}>
+            <Card.Header style={{ display: "flex" }}>
+              <span
+                style={{
+                  color: "black",
+                  textDecoration: "none",
+                  flex: 1,
+                  cursor: "pointer",
+                  alignSelf: "center",
+                  fontSize: 18,
+                }}
+              >
+                {note.title}
+              </span>
+              <div>
+                <Button>Edit</Button>
+                <Button variant="danger" className="mx-2">
+                  Delete
+                </Button>
+              </div>
+            </Card.Header>
+          </Card>
+        ))}
+        
+      
     </MainScreen>
-  )};
+  );
+};
 
 export default MyNotes;
