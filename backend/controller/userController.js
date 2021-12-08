aec267da522dcd659b40a788705c03c45a0e0f7f
Prashant Goswami <87839549+prashantcoding@users.noreply.github.com>
2021-12-08 19:02:44 +0530

    jwsToken

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler=require('express-async-handler')
 const User=require('../models/userModel')
+const generateToken = require('../utils/generateToken')
 const registerUser=asyncHandler(async(req,res)=>{
     const{name,email,password,pic}=req.body;
     const userExists=await User.findOne({email});
@@ -15,6 +16,7 @@ const registerUser=asyncHandler(async(req,res)=>{
             email:user.email,
             isAdmin:user.isAdmin,
             pic:user.pic,
+            token:generateToken(user._id),
         });
     }
 });
@@ -28,10 +30,11 @@ const authUser=asyncHandler(async(req,res)=>{
             email:user.email,
             isAdmin:user.isAdmin,
             pic:user.pic,
+            token:generateToken(user._id),
         });
     }else{
         res.status(400)
-        throw new Error("Error Occured!")
+        throw new Error("Invalid Credentials")
     }
     
 });
