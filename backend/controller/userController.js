772474dece34740c20dc0b74a6aa7a7288655976
Prashant Goswami <87839549+prashantcoding@users.noreply.github.com>
2021-12-08 13:58:36 +0530

    created controller

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,8 +1,13 @@
-const registerUser=async(req,res)=>{
+const asyncHandler=require('express-async-handler')
+const User=require('../models/userModel')
+const registerUser=asyncHandler(async(req,res)=>{
     const{name,email,password,pic}=req.body;
-    res.json({
-        name,
-        email,
-    });
-}
+    const userExists=await User.findOne({email});
+    if(userExists) {
+        res.send("USer exits")
+    }
+    else{
+        res.send("User does not exist")
+    }
+})
 module.exports={registerUser};
