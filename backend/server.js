a6747d70cfc6a2a1724e64917b5f573b460c9163
Prashant Goswami <87839549+prashantcoding@users.noreply.github.com>
2021-12-08 13:45:20 +0530

    creaeted Routes

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,15 @@ const notes=require('./data/notes');
 const dotenv = require('dotenv')
 const app=express();
 const connectDB=require('./config/db')
+const userRoutes=require('./routes/userRoutes')
 dotenv.config();
 connectDB();
-
+app.use(express.json());
 app.get('/',(req,res) => {
     res.send("API IS WORKING....")
 });
 app.get('/api/notes',(req,res) => {
     res.json(notes);
 })
+app.use('/api/users',userRoutes)
 app.listen(5000,console.log("Server Started on PORT 5000"));
