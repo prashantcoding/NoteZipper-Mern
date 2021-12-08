02d7a44271057ed9e865250a30e0043bdb7de99c
Prashant Goswami <87839549+prashantcoding@users.noreply.github.com>
2021-12-08 20:20:34 +0530

    User Login Using Axiox

--- frontend/src/Screens/LoginScreen/LoginScreen.js
+++ frontend/src/Screens/LoginScreen/LoginScreen.js
@@ -1,33 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import MainScreen from "../../component/MainScreen/MainScreen";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import './loginS.css'
+import "./loginS.css";
+const axios = require('axios');
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("false");
+  const [loading, setLoading] = useState("false");
+  const submitHandler =async (e) => {
+    e.preventDefault();
+    try {
+        const config={
+            headers: { 'Content-Type': 'application/json'}
+        }
+        setLoading(true)
+        const {data} = await axios.post('/api/users/login',{
+            email,password
+        },config)
+      console.log(data);
+    } catch (error) {
+        setError(error.response.data.message)
+        console.log(error)
+    }
+  };
   return (
     <MainScreen title="LOGIN">
       <div className="loginContainer">
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+        <Form onSubmit={submitHandler}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-      <Row className="py-3">
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              value={password}
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group
+            className="mb-3"
+            controlId="formBasicCheckbox"
+          ></Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        <Row className="py-3">
           <Col>
             New Customer ? <Link to="/register">Register Here</Link>
           </Col>
